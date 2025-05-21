b4f8d922b39ef1cdb98215a25e0f893076e954f5
James <s223088420@gmail.com>
2025-05-21 11:39:01 +0200

    3: Added SayGoodbye

--- GitDemo/src/Main.java
+++ GitDemo/src/Main.java
@@ -2,4 +2,10 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
+
+    public void SayGoodbye()
+    {
+	// This prints to the standard output     
+        System.out.println("Say Goodbye);
+    }
 }
