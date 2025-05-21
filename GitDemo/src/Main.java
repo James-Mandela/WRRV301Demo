1fedaf98eed0b810ab37a6ecec1b10043f18e7dc
James <s223088420@gmail.com>
2025-05-21 11:45:59 +0200

    Say First and Last Name

--- GitDemo/src/Main.java
+++ GitDemo/src/Main.java
@@ -3,14 +3,18 @@ public class Main {
         System.out.println("Hello, World!");
         SayHello();
     }
-    public void SayHello()
+    public void SayHello(String FirstName, String LastName)
     {
         System.out.println("Hello There");
+        System.out.println(FirstName);
+        System.out.println(LastName);
     }
 
-    public void SayGoodbye()
+    public void SayGoodbye(String FirstName, String LastName)
     {
 	// This prints to the standard output     
         System.out.println("Say Goodbye);
+        System.out.println(FirstName);
+        System.out.println(LastName);
     }
 }
